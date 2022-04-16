7d4fc19c2dc079e02094d18427136826265bf10e
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 14:25:39 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("C")
+    basic.showString("C")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+    basic.showString("B")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("D")
+    basic.showString("D")
+})
+radio.setGroup(1)
+radio.setTransmitSerialNumber(true)
+basic.forever(function () {
+	
+})
